311a12e65291b7998918873185322857b921f994
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-09-22 16:32:23 +0530

    functions in js :art:

--- tut53.js
+++ tut53.js
@@ -1,18 +1,32 @@
 console.log("this is a tutorial");
 
-function greet(name)
+function greet(name, greettext="greetings from js") // here it is default value
 {
+    let name1="bitch" // local variables
     // console.log()
+    console.log(greettext +" "+name)
     console.log(name + " is a welcomed");
 }
+
+function sum(a,b,c){
+    let d=a+b+c;
+    return d; //returning a value
+    // console.log("this is unreachable code") this will never execute
+}
+
+let s=sum(3,6,2);
+console.log(s)
 // let name="harry";
+
 let name0="saurav";
 let name1="booboo";
 let name2="huihui";
-
-greet(name0)
+let text="good morning";
+greet(name0,text)
 greet(name1)
-greet(name2)
+greet(name2,text)
+
+
 
 
 
