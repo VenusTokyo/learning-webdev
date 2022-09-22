a459901e8dcae19f443a44a9eab63cb44cb86227
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-09-22 16:19:37 +0530

    if else and switch :beers:

---
+++ tut53.js
@@ -0,0 +1,19 @@
+console.log("this is a tutorial");
+
+function greet(name)
+{
+    // console.log()
+    console.log(name + " is a welcomed");
+}
+// let name="harry";
+let name0="saurav";
+let name1="booboo";
+let name2="huihui";
+
+greet(name0)
+greet(name1)
+greet(name2)
+
+
+
+
