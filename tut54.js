7b6dd7c0490442e73a428ceb3d8110df5a007c8b
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-09-22 16:56:58 +0530

    alert prompt confirm :art:

---
+++ tut54.js
@@ -0,0 +1,31 @@
+// aleart in browser js
+
+// alert("This is a message") 
+//will take only a string as argument, unless you closes this u cannot interact with page (modal)
+//aleart does not return anything
+
+
+//prompt
+// let name = prompt("What is your name?","guest");
+//guest id default argument
+// console.log(name);
+//prompt returns string or undedefined
+
+
+//confirm
+// let deletePost= confirm("Do you really want to delete this post??")
+// console.log(deletePost)
+//confirm returns boolean values
+
+//challenge
+let age=prompt("Enter your age");
+
+if(age>=18)
+{
+    console.log("You're allowed in the club")
+}
+else
+{
+    console.log("You're not allowed in the club")
+}
+    
