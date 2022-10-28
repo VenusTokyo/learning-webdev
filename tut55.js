998b9f87084841cd60dd174336a4102081607894
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-10-28 15:56:48 +0530

    revising loops in js

--- tut55.js
+++ tut55.js
@@ -5,49 +5,52 @@
 //     console.log(i); 
 // }
 
-let friend =["rohan", "sanjeev","saurav","jyoti"];
+let friend = ["rohan", "sanjeev", "saurav", "jyoti"];
 for (let index = 0; index < friend.length; index++) {
-    console.log("hello friend " +friend[index]);
-    
+    console.log("hello friend " + friend[index]);
+
 }
 
 //for each loop
-friend.forEach(function f(element)
-{
-    console.log('hello frraandss ' + element+ " to mordern js")
+friend.forEach(function f(element) { 
+    // function f runs for every element of friend array. this kinf of looop os best for iterating arrays
+
+    console.log('hello frraandss ' + element + " to mordern js")
 });
 
 //for of loop
-for ( element of friend) {
-    console.log('hello frraandss ' + element+ " to mordern js")
+for (element of friend) {
+    //simpler version of forEach loop
+    console.log('hello frraandss ' + element + " to mordern js")
 
-    
+
 }
 
-let employee={
-    name:"harry",
-    salary:2,
-    boobs:"none"
+let employee = {
+    name: "harry",
+    salary: 2,
+    boobs: "none"
 
 }
 
 //for in loop for iteration in objects
 for (key in employee) {
-    console.log(`the ${key} of employee is ${employee[key]}` );
+    //for iteration in an object  for in loop is best
+    console.log(`the ${key} of employee is ${employee[key]}`);
 
     // use ` sign
 }
 
 
 //while loop
-let i=0;
-while (i<4) {
-    console.log(`${i} is less than 4`)
+let i = 0;
+while (i < 4) {
+    console.log(`${i} is less than 4`) //${i} is for indicating its the variable and not string and will onlu work when enclosed within `
     i++;
 }
-let j=34
+let j = 34
 //do while loop
 do {
     console.log(`${j} is less than4`)
     i++;
-} while (i<4);
+} while (i < 4);
