2e1148b4294f596f903a62ee72ec1008efef5bfd
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-10-31 16:11:49 +0530

    date and time

---
+++ tut59.js
@@ -0,0 +1,14 @@
+console.log("this is a date and time tutotrail");
+let now=new Date();
+console.log(now)
+
+let dt = new Date(0)
+console.log(dt)
+
+let newDate= new Date("2029-09-30")
+console.log(newDate)
+
+let yr =newDate.getFullYear()
+console.log(yr)
+let a= newDate.getUTCDate()
+console.log(a)
