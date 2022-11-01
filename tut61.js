c9d28fe31c873dc16fac63f0d4a37ecd97c1ecb1
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-11-01 16:31:12 +0530

    math obj :sparkles:

---
+++ tut61.js
@@ -0,0 +1,23 @@
+let m=Math;
+console.log(m)
+
+console.log(Math.E)
+console.log(Math.PI)
+
+let a=78.7348543
+
+console.log(Math.round(a))
+console.log(Math.pow(5,3))
+console.log(Math.sqrt(81))
+
+//genarating a random no.
+let r=Math.random()
+//randome fn genrates a no. btw 0 and 1
+console.log(r)
+
+let min=1
+let max=100
+let rand=min +(max-min)*Math.random();
+console.log(rand)
+
+
