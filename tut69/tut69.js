409a9922855e16b89a007dcebf911993c6160045
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2022-11-07 20:09:26 +0530

    installing packages

---
+++ tut69/tut69.js
@@ -0,0 +1,28 @@
+console.log("this is tutorial 69")
+//npm is node package manager
+
+//command to make a package.json file 
+// npm init
+//wee can insatll packages by 
+//npm install express
+//package is just some open source functionalitiees and feature written by someone else that we can utilize in our projects
+
+//node modules folder stores all the dependencies we are gonna use in our project 
+
+//if we want to install some special version of a package the we write
+// npm install slugify@1.3.5
+
+// 1.3.5
+//major.minor.patch
+
+//npm install nodemon --save-dev 
+//this is command from installling something that is only required in dev pahse and not in production phase 
+
+//npm install nodemon --global
+//global to access from all
+
+//npm i <package name>
+//i shot of install
+
+//^ mean install exactly that version > for that version or above
+//npm view slugify version
